39130b8dce845437d233f606b7774a4fbffc8e48
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2015-02-24 12:01:56 +0000

    Ensure project egg is in Python path

--- temmpo/wsgi.py
+++ temmpo/wsgi.py
@@ -7,9 +7,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
 os.environ["DJANGO_SETTINGS_MODULE"] = "temmpo.settings.dev"
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+sys.path.insert(0, PROJECT_ROOT)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
