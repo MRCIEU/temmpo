15b1010d2d5b9eb18518cf0b7fef7f50642bae68
Kieren Pitts <kieren.pitts@bristol.ac.uk>
2015-03-27 22:07:25 +0000

    Updated comments for reference

--- temmpo/prepopulate/pre_populate.py
+++ temmpo/prepopulate/pre_populate.py
@@ -6,16 +6,16 @@ def pre_populate_genes():
     Pre-populate the genes from the available files
     """
     this_path = os.path.dirname(__file__)
-    # I think the supplied file has integrity errors in that genes that are 
-    # listed as synonyms also appear in their own right (hence the code to 
+    # I think the supplied file has integrity errors in that genes that are
+    # listed as synonyms also appear in their own right (hence the code to
     # check we don't aalready have records)
-    # wc -l  Homo_sapiens.gene_info == 
-    # Gene.objects.filter(synonym_for__exact = None).count()
+    # wc -l  Homo_sapiens.gene_info == 43855
+    # Gene.objects.filter(synonym_for__exact = None).count() == 43257
     file_list = ['Homo_sapiens.gene_info',]
-    
-    for file in file_list:   
+
+    for file in file_list:
         genefile = open("%s/%s" % (this_path, file),'r')
-        
+
         for line in genefile:
             line = line.strip().split()
             genename = line[2]
@@ -24,14 +24,14 @@ def pre_populate_genes():
                 synonyms = []
             else:
                 synonyms = line[4].split("|")
-                
+
             # Store gene
             if Gene.objects.filter(name = genename).exists():
                 this_gene = Gene.objects.get(name = genename)
             else:
                 this_gene = Gene(name = genename)
                 this_gene.save()
-            
+
             for synonym in synonyms:
                 if not Gene.objects.filter(name = synonym).exists():
                     new_synonym = Gene(name = synonym,
@@ -41,7 +41,6 @@ def pre_populate_genes():
 #                 else:
 #                     # Exists
 #                     parent_gene = Gene.objects.get(name = synonym)
-                    
-            
+
+
         genefile.close()
-    
