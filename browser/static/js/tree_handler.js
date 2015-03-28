353741dacf174b31ec7747942de7aa6e8234efde
Kieren Pitts <kieren.pitts@bristol.ac.uk>
2015-03-28 20:41:29 +0000

    Tidied up tree handler. Mainly removed commented out code

--- browser/static/js/tree_handler.js
+++ browser/static/js/tree_handler.js
@@ -1,9 +1,4 @@
-// Set up JStree
-// Initialise the JSTree instance
-//$(function () { $('#term_tree').jstree(); });
-console.log('INIT');
-// Add function to record change
+// Add function to record change via jsTree
 $('#term_tree').on("changed.jstree", function (e, data) {
-    console.log(data.selected);
     $('#term_data').val(data.selected);
 });
