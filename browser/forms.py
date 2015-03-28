27cbcde04f9e0cc089212b3535f0e830b4ceb5f8
Kieren Pitts <kieren.pitts@bristol.ac.uk>
2015-03-28 17:55:34 +0000

    Added fields to form and also created a clean method

--- browser/forms.py
+++ browser/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from browser.models import SearchCriteria, Upload, SearchResult
+from browser.models import SearchCriteria, Upload, SearchResult, MeshTerm
 
 
 class AbstractFileUploadForm(forms.ModelForm):
@@ -15,10 +15,31 @@ class AbstractFileUploadForm(forms.ModelForm):
 
 
 class ExposureForm(forms.ModelForm):
+    term_data = forms.CharField(widget=forms.HiddenInput,
+                                required=True)
+
+    selected_tree_root_node_id = forms.CharField(widget=forms.HiddenInput,
+                                         required=False)
+    btn_submit = forms.CharField(widget=forms.HiddenInput)
+
     class Meta:
         model = SearchCriteria
-        fields = ['exposure_terms', ]
+        fields = ['term_data', 'selected_tree_root_node_id','btn_submit']
 
+    def clean(self, *args, **kwargs):
+        cleaned_data = super(ExposureForm, self).clean()
+        print "Exposure clean", cleaned_data, args, kwargs
+
+        if 'term_data' in cleaned_data:
+            all_terms = cleaned_data['term_data'].split(',')
+            cleaned_data['exposure_terms'] = MeshTerm.objects.filter(tree_number__in = all_terms)
+
+        return cleaned_data
+
+        #data = dict(self.data)
+        #print data
+        #if 'content' in data:
+        #    print data['content']
 
 class MediatorForm(forms.ModelForm):
     class Meta:
