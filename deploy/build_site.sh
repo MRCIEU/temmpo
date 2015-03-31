579b7917bd40a037b985e41357f0fade6702543d
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2015-03-31 11:51:06 +0100

    Adjust dev build script to take parameter for ve name; Sym link in lxml before installing other dependent eggs; Add more outpus related to location of commands being run;

--- deploy/build_site.sh
+++ deploy/build_site.sh
@@ -1,17 +1,32 @@
 # Build script for TeMMPo
+# Accept argument for VE - defaults to beta
 MY_USER=$(whoami)
 BASEPATH="/usr/local/projects/tmma/lib/"$MY_USER
-if [[ ! -d "$BASEPATH" ]]; then
+VE="beta"
+
+if [ ! -d "$BASEPATH" ]; then
   echo "Create directory"
-  mkdir "$BASEPATH"
+  mkdir $BASEPATH
 else
   echo "Directory exists"
 fi
 
-cd "$BASE_PATH"
+cd $BASEPATH
+pwd
 
-virtualenv --no-site-packages beta
-cd beta
+# Allow override of virtual environment name
+if [ -n "$1" ]; then
+    VE=$1
+    echo "Overriding the default virtual environment name with:"
+    echo $VE
+fi
+
+echo "Create virtual environment in this location:"
+pwd
+virtualenv --no-site-packages $VE
+cd $VE
+pwd
+echo "Create required sub directories"
 mkdir etc
 mkdir share
 mkdir src
@@ -20,29 +35,41 @@ mkdir var
 # Clone git repo
 echo "Clone repo"
 cd src
+pwd
 git clone git.ilrt.bris.ac.uk:/usr/local/projects/git/projects/temmpo
+
+# Sym link in core libraries - in particular any that require compilation
+cd $BASEPATH
+cd $VE
+cd ./lib/python2.7/site-packages/
+pwd
+ln -s /usr/lib/python2.7/dist-packages/lxml lxml
+ln -s /usr/lib/python2.7/dist-packages/lxml-2.3.2.egg-info lxml-2.3.2.egg-info
+
+# TODO consider using core version of Django on the box
 
 # Load requirements
 echo "Load requirements"
-../bin/pip install -r temmpo/deploy/project-eggs-freeze.txt
-../bin/pip install -r temmpo/deploy/now.txt
+cd $BASEPATH
+cd $VE
+cd src/temmpo
+pwd
+../../bin/pip install -r deploy/project-eggs-freeze.txt
+../../bin/pip install -r deploy/now.txt
 
 #bin/pip install django # Tested with 1.7.4
-#bin/pip install django-chunked-upload # Trailing
-#bin/pip install django-bft # Trailing
+#bin/pip install django-chunked-upload # Trialing
+#bin/pip install django-bft # Trialing
 #bin/pip install recaptcha-client
-
-cd ../lib/python2.7/site-packages/
-ln -s /usr/lib/python2.7/dist-packages/lxml lxml
-ln -s /usr/lib/python2.7/dist-packages/lxml-2.3.2.egg-info lxml-2.3.2.egg-info
 
 # DB
 echo "Creating SQLite db"
 cd $BASEPATH
-cd beta/src/temmpo
+cd $VE
+cd src/temmpo
+pwd
 ../../bin/python manage.py migrate
 
 # Create a super user
 echo "Create superuser"
- ../../bin/python manage.py createsuperuser --settings=temmpo.settings.dev
-
+../../bin/python manage.py createsuperuser --settings=temmpo.settings.dev
