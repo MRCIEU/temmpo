aaf819ab9044cdc21d152d183f78988f5ca01a4f
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2015-05-21 10:39:53 +0100

    Record dev only eggs

--- temmpo/settings/dev.py
+++ temmpo/settings/dev.py
@@ -1,1 +1,21 @@
 from base import *
+
+DEBUG_TOOLBAR_PANELS = [
+    # 'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    # 'debug_toolbar.panels.headers.HeadersPanel',
+    # 'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    # 'debug_toolbar.panels.signals.SignalsPanel',
+    # 'debug_toolbar.panels.logging.LoggingPanel',
+    # 'debug_toolbar.panels.redirects.RedirectsPanel',
+    'template_timings_panel.panels.TemplateTimings.TemplateTimings',
+]
+
+
+DEV_APPS = ['debug_toolbar', 'template_timings_panel',]
+# INSTALLED_APPS.extend(DEV_APPS)
