cc60e694b0c37331d9a971299e47153c03145c25
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2015-05-22 13:49:34 +0100

    Add fix to preserve previously selected nodes that are not visible on the tree

--- browser/static/js/tree_handler.js
+++ browser/static/js/tree_handler.js
@@ -1,15 +1,35 @@
 // Add function to record change via jsTree
 $('#term_tree').on("changed.jstree", function (e, data) {
 
-    // TODO: BUG: currently overwrites any selected noted that have not been loaded yet, appears you cannot record seleted for items that have not been loaded yet.
+    // on change - merge selected tree nodes with subset of previously selected nodes that do not exist in the tree 
 
-    // IDEA: Load all selected and ancestor nodes on first creation of tree, NB: This would reduce performanace - update views.py
-
-    // NB: state plugin seemed to have the same problem
-
-    // IDEA: For nodes that tree does not have loaded in term_data - preserve and merge with currently selected - update js
-
-    // on change - merge selected tree nodes with subset of previously nodes that do not exist in the tree - possible check if undetermined nodes is an ancestor ?? possible can compare using tree_number string comparison
     // TODO: Nodes that have children when selected should also select all child nodes - action in views form handler
-    $('#term_data').val(data.selected);
+
+    var previously_selected_nodes = $('#term_data').val().split(","); ;
+    var unloaded_selected_nodes = [];
+
+    for (i = 0; i < previously_selected_nodes.length; i++) {
+    	node = previously_selected_nodes[i];
+
+    	obj_tree = data.instance.get_node(node); //$('#term_tree').get_node(node);
+    	console.log("node");
+		console.log(node);
+    	console.log("obj_tree");
+    	console.log(obj_tree);
+    	if (!obj_tree){
+    		unloaded_selected_nodes.push(node);
+    		console.log("attempt push node");
+    	}
+	}
+	// console.log("data.selected");
+	// console.log(data.selected);
+	// console.log("unloaded_selected_nodes");
+	// console.log(unloaded_selected_nodes);
+	unloaded_selected_nodes_str = unloaded_selected_nodes.join()
+
+	if (unloaded_selected_nodes_str &&  data.selected){
+		$('#term_data').val(data.selected + "," + unloaded_selected_nodes_str);
+	} else {
+    	$('#term_data').val(data.selected);
+	}	
 });
