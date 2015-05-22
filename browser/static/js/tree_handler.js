242a67522e84d2fdd638ce436fd59feb10f4d6ed
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2015-05-22 06:18:21 +0100

    Update notes on how to tackle bug

--- browser/static/js/tree_handler.js
+++ browser/static/js/tree_handler.js
@@ -1,9 +1,12 @@
 // Add function to record change via jsTree
-// TODO : Only parent node is being selected
 $('#term_tree').on("changed.jstree", function (e, data) {
-    // If the item changing is a previously saved item and no longer selected remove from term_data
 
-    // else If changing a new item merge data.selected with current term_data
+    // TODO: BUG: currently overwrites any selected noted that have not been loaded yet, appears you cannot record seleted for items that have not been loaded yet.
+
+    // IDEA: Load all selected and ancestor nodes on first creation of tree, NB: This would reduce performanace - update views.py
+
+    // NB: state plugin seemed to have the same problem
+
+    // IDEA: For nodes that tree does not have loaded in term_data - preserve and merge with currently selected - update js
     $('#term_data').val(data.selected);
-    // TODO: BUG: currently overwrites any selected noted that have not been loaded yet
 });
