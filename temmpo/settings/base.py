66873ca025d51bccd7a82d473e88d4a007fd2b71
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2015-12-08 17:49:53 +0000

    Update comments

--- temmpo/settings/base.py
+++ temmpo/settings/base.py
@@ -2,10 +2,10 @@
 Django settings for temmpo project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
+https://docs.djangoproject.com/en/1.8/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
+https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,7 +22,7 @@ VEDIR = APP_ROOT.split('/')[-1]  # demo, alpha etc.
 ADMINS = (('Tessa Alexander', 'cmtsa@ilrt.org'),)
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fva(zwxn6)g6bk$46e=(_b-5&*y1%!jd*hrn+yke91g@i#%dj%'
@@ -34,7 +34,7 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '137.222.16.18',
                  '.ilrt.bris.ac.uk', '.ilrt.bristol.ac.uk',
                  '.bris.ac.uk', '.bristol.ac.uk', 'temmpo.org.uk',
-                 'www.temmpo.org.uk',]
+                 'www.temmpo.org.uk', ]
 
 APACHE = False
 
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'temmpo.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -81,7 +81,7 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-gb'
 
@@ -96,7 +96,7 @@ USE_TZ = True
 SHORT_DATE_FORMAT = 'd/m/Y'
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
@@ -121,7 +121,7 @@ LOGGING = {
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
-         }
+        }
     },
     'formatters': {
         'verbose': {
@@ -170,8 +170,8 @@ LOGGING = {
         },
     },
     'root': {
-             'handlers': ['console', 'local_file'],
-             'level': 'DEBUG',
+        'handlers': ['console', 'local_file'],
+        'level': 'DEBUG',
     }
 }
 
