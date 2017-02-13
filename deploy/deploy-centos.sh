cedd783cd7513e1252b2a9903ceae3b626726a59
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2017-02-13 19:18:54 +0000

    Update Centos Vagrant builds to be used whilst developing template changes - NB production site has not been migrated to centos as yet.

--- deploy/deploy-centos.sh
+++ deploy/deploy-centos.sh
@@ -2,10 +2,11 @@ echo "Build script for TeMMPo"
 
 sudo yum -y install epel-release
 sudo yum -y update
-
 sudo yum -y install python-devel
 sudo yum -y install python-wheel
 sudo yum -y install python-magic
+
+sudo yum -y install python-virtualenv
 sudo yum -y install libxml2-python
 sudo yum -y install libxml2-devel
 sudo yum -y install libxslt-python
@@ -23,27 +24,31 @@ sudo yum -y install git
 sudo yum -y install nano
 
 # Web server setup
-# httpd mod_wsgi cronolog
+sudo yum -y install httpd 
+sudo yum -y install mod_wsgi 
+# sudo yum -y install cronolog  # TODO: Add to production Centos puppet config
 
 # Python application dependencies
 
-echo "Install virtual env with pip"
-sudo pip install virtualenv
-
 echo "Create virtual environment in this location:"
-cd /var/local/
-virtualenv --system-site-packages -p /usr/bin/python2 temmpo
-cd temmpo
+mkdir -p /srv/projects/temmpo/lib/
+cd /srv/projects/temmpo/lib/
+# Ensure use Python 2.7 is used
+virtualenv --system-site-packages -p /usr/bin/python2 dev
+cd dev
 ls -l
 echo "Create required sub directories"
-mkdir -p etc
-mkdir -p src
-mkdir -p var/results
-mkdir -p logs
+mkdir -p /srv/projects/temmpo/etc
+# TODO: Non vagrant build need to create and clone the repo in the src directory
+# mkdir -p src
+# cd src 
+# git clone git@bitbucket.org:researchit/temmpo.git
+mkdir -p /srv/projects/temmpo/lib/dev/var/results
+mkdir -p /srv/projects/temmpo/lib/dev/var/abstracts
 
 echo "Load base application requirements"
-cd src/temmpo
-../../bin/pip install -r requirements/base.txt
+cd /srv/projects/temmpo/lib/dev/
+./bin/pip install -r src/temmpo/requirements/centos-dev.txt
 
 # # contexts that are required for application to run behind Apache
 # # TODO: review if semanage should be used instead
@@ -52,5 +57,26 @@ cd src/temmpo
 # sudo chcon -R -t httpd_sys_rw_content_t /var/local/temmpo/var
 # sudo chcon -R -t httpd_sys_script_exec_t /var/local/temmpo/lib/python2.7/site-packages/
 
-# # Production excptions - clone prod stable
+# # Production exceptions - clone prod stable
 # # NB: Would require requirements/prod.txt
+
+cd /srv/projects/temmpo/lib/
+chown --silent -R vagrant:vagrant dev
+
+
+echo "Creating SQLite database"
+cd /srv/projects/temmpo/lib/dev/src/temmpo
+../../bin/python manage.py migrate --settings=temmpo.settings.dev
+
+echo "MANUAL STEP: Still need to run - Create superuser"
+../../bin/python manage.py createsuperuser --settings=temmpo.settings.dev
+
+echo "Run tests"
+../../bin/python manage.py test --settings=temmpo.settings.dev
+
+echo "Run dev server"
+../../bin/python manage.py runserver 0.0.0.0:59099 --settings=temmpo.settings.dev
+
+echo "Open http://127.0.0.1:59099 in your local web browser"
+
+echo "TODO populate the mesh terms"
