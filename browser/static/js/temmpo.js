544508520b548e59d269dfb931ae11d2ece51545
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2017-02-13 23:54:13 +0000

    Refactor form HTML into an include; Use minimised versions of CSS/JS; Decorate HTML form fields with required JS

---
+++ browser/static/js/temmpo.js
@@ -0,0 +1,4 @@
+// $('.form-group label').addClass('control-label');
+$('.form-group input').addClass('form-control');
+$('.form-group select').addClass('form-control');
+$('.form-group textarea').addClass('form-control');
