745a59d4e51ef2f70db06f50d5532a598e033efd
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2017-02-20 11:00:20 +0000

    Remove unused code and comments

--- browser/static/js/tree_handler.js
+++ browser/static/js/tree_handler.js
@@ -3,8 +3,6 @@ $('#term_tree').on("changed.jstree", function (e, data) {
 
     // on change - merge selected tree nodes with subset of previously selected nodes that do not exist in the tree 
 
-    // TODO: Nodes that have children when selected should also select all child nodes - action in views form handler
-
     var previously_selected_nodes = $('#term_tree_ids').val().split(","); ;
     var unloaded_selected_nodes = [];
 
@@ -21,10 +19,7 @@ $('#term_tree').on("changed.jstree", function (e, data) {
     		console.log("attempt push node");
     	}
 	}
-	// console.log("data.selected");
-	// console.log(data.selected);
-	// console.log("unloaded_selected_nodes");
-	// console.log(unloaded_selected_nodes);
+
 	unloaded_selected_nodes_str = unloaded_selected_nodes.join()
 
 	if (unloaded_selected_nodes_str &&  data.selected){
