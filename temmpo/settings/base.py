fc9a7db1cbe48e47aca60e9f5cd861ee2c278d7b
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2017-04-03 17:33:34 +0100

    TMMA-191 Address Django security warnings

--- temmpo/settings/base.py
+++ temmpo/settings/base.py
@@ -69,6 +69,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'temmpo.urls'
