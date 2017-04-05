a8be010a6a5923671b27999bc5832797917e447e
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2017-04-05 18:46:43 +0100

    Ensure dev Apache build use their own settings files

--- temmpo/wsgi.py
+++ temmpo/wsgi.py
@@ -10,10 +10,14 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 import sys
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "temmpo.settings.prod"
-
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 sys.path.insert(0, PROJECT_ROOT)
 
+if "dev" in PROJECT_ROOT:
+    os.environ["DJANGO_SETTINGS_MODULE"] = "temmpo.settings.dev"
+else:
+    os.environ["DJANGO_SETTINGS_MODULE"] = "temmpo.settings.prod"
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
