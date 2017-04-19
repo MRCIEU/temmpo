c12a4504538303fd625bba6b5a3adf49e345656a
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2017-04-19 15:56:10 +0100

    Bug fix DB name

--- deploy/db-setup.sql
+++ deploy/db-setup.sql
@@ -2,8 +2,8 @@ DROP DATABASE IF EXISTS interact;
 CREATE DATABASE temmpo_d CHARACTER SET UTF8;
 CREATE USER temmpo@'%' IDENTIFIED BY 'notsosecret';
 CREATE USER temmpo_a@'%' IDENTIFIED BY 'notsosecret_a';
-GRANT SELECT, INSERT, UPDATE, DELETE ON interact_p.* TO temmpo@'%';
-GRANT ALL PRIVILEGES ON interact_p.* TO temmpo_a@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON temmpo_d.* TO temmpo@'%';
+GRANT ALL PRIVILEGES ON temmpo_d.* TO temmpo_a@'%';
 
 # Minimum MySQL security setting from mysql_secure_installation to allow development via remote server
 DELETE FROM mysql.user WHERE User='';
