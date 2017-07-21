53cd3624ea635dc2d8c58dc87d11669d58129122
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2017-07-21 17:55:51 +0100

    Ensure test runner does not create a test database for the legacy sqlite db

--- temmpo/settings/test_mysql.py
+++ temmpo/settings/test_mysql.py
@@ -1,5 +1,12 @@
 from base import *
 
-# Ensure test runner does not create a test database for the admin user's credential set
-if DATABASES and DATABASES.has_key('admin'):
-	del DATABASES['admin']
+if DATABASES:
+
+	# Ensure test runner does not create a test database for the admin user's credential set
+	if DATABASES.has_key('admin'):
+		del DATABASES['admin']
+
+	# Ensure test runner does not create a test database for the legacy sqlite db
+	if DATABASES.has_key('sqlite'):
+		del DATABASES['sqlite']
+
