0cb56503befe4484d573bad357fd8c613c9362bb
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2017-12-27 23:05:04 +0000

    Change taging and merging functionaility to be handled by the Jenkins CI job instead. Will move to date based tagging.

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -49,43 +49,43 @@ def _toggle_local_remote(use_local_mode):
     return (caller, change_dir)
 
 
-def taggit(gfrom='master', gto='', egg='', msg='Marking for release'):
-    """fab taggit:master,0.9,egg Create a tag or move a branch.
-        Example create a tag and move it to stable...
-        taggit:master,0.8 then taggit:master,prod_stable
-    """
-    if not (gfrom and gto and egg):
-        print 'You must specify taggit:from,to,egg'
-        return
-    try:
-        testint = int(gfrom[0])
-        print 'Don\'t move numeric tags please - add a new one!'
-        return
-    except:
-        pass
-    try:
-        testint = int(gto[0])
-        print 'Marking release tag %s on branch %s' % (gto, gfrom)
-        action = 'tag'
-    except:
-        print 'Merging branch %s to branch %s' % (gto, gfrom)
-        action = 'merge'
-    with lcd(GIT_DIR + egg):
-        local('git pull')
-        if action == 'merge':
-            # make sure this is up to date branches before merging
-            local('git checkout %s' % gfrom)
-            local('git pull')
-            local('git checkout %s' % gto)
-            local('git pull')
-            local('git merge -m "merge for release tagging" %s' % gfrom)
-            local('git push')
-        else:
-            local('git checkout %s' % gfrom)
-            local('git tag -a %s -m "%s"' % (gto, msg))
-            local('git push origin %s' % gto)
-        # Reset local git repo to master
-        local('git checkout master')
+# def taggit(gfrom='master', gto='', egg='', msg='Marking for release'):
+#     """fab taggit:master,0.9,egg Create a tag or move a branch.
+#         Example create a tag and move it to stable...
+#         taggit:master,0.8 then taggit:master,prod_stable
+#     """
+#     if not (gfrom and gto and egg):
+#         print 'You must specify taggit:from,to,egg'
+#         return
+#     try:
+#         testint = int(gfrom[0])
+#         print 'Don\'t move numeric tags please - add a new one!'
+#         return
+#     except:
+#         pass
+#     try:
+#         testint = int(gto[0])
+#         print 'Marking release tag %s on branch %s' % (gto, gfrom)
+#         action = 'tag'
+#     except:
+#         print 'Merging branch %s to branch %s' % (gto, gfrom)
+#         action = 'merge'
+#     with lcd(GIT_DIR + egg):
+#         local('git pull')
+#         if action == 'merge':
+#             # make sure this is up to date branches before merging
+#             local('git checkout %s' % gfrom)
+#             local('git pull')
+#             local('git checkout %s' % gto)
+#             local('git pull')
+#             local('git merge -m "merge for release tagging" %s' % gfrom)
+#             local('git push')
+#         else:
+#             local('git checkout %s' % gfrom)
+#             local('git tag -a %s -m "%s"' % (gto, msg))
+#             local('git push origin %s' % gto)
+#         # Reset local git repo to master
+#         local('git checkout master')
 
 
 def make_virtualenv(env="dev", configure_apache=False, clone_repo=False, branch=None, migrate_db=True, use_local_mode=False, requirements="base"):
@@ -142,10 +142,8 @@ def make_virtualenv(env="dev", configure_apache=False, clone_repo=False, branch=
         setup_apache(env, use_local_mode)
 
 
-def deploy(env="dev", branch="master", using_apache=True, tag='', merge_from='', migrate_db=True, use_local_mode=False, use_pip_sync=False, requirements="base"):
-    """NB: env = dev|prod.  Optionally tag and merge the release env="dev", branch="master", using_apache=True, tag='', merge_from='', migrate_db=True, use_local_mode=False, use_pip_sync=False, requirements="base"."""
-    # TODO: Tagging and merging branches needs an implementation review.
-
+def deploy(env="dev", branch="master", using_apache=True, migrate_db=True, use_local_mode=False, use_pip_sync=False, requirements="base"):
+    """NB: env = dev|prod.  Optionally tag and merge the release env="dev", branch="master", using_apache=True, migrate_db=True, use_local_mode=False, use_pip_sync=False, requirements="base"."""
     # Convert any string command line arguments to boolean values, where required.
     using_apache = (str(using_apache).lower() == 'true')
     migrate_db = (str(migrate_db).lower() == 'true')
@@ -158,12 +156,6 @@ def deploy(env="dev", branch="master", using_apache=True, tag='', merge_from='',
 
     if using_apache:
         disable_apache_site(use_local_mode)
-
-    if tag:
-        taggit(gfrom="master", gto=tag, egg='temmpo')
-
-    if merge_from:
-        taggit(gfrom=merge_from, gto=branch, egg='temmpo')
 
     src_dir = PROJECT_ROOT + "lib/" + env + "/src/temmpo/"
 
