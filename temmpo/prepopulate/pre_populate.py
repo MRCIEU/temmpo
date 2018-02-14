4e9425f2bcc759126b37ca5ba0ed1f18caf28e7e
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2018-02-14 13:32:51 +0000

    Convert gene import helper function to use a fiel path from the settings file instead of a hardcoded value; Remove file list functionality which was never used; Bug fix importing the file header as gene information.

--- temmpo/prepopulate/pre_populate.py
+++ temmpo/prepopulate/pre_populate.py
@@ -1,40 +1,47 @@
+"""Helper function to create Genes based on a supplied or default gene file."""
+
 from browser.models import Gene
-import os
+from django.conf import settings
+
 
 def pre_populate_genes():
-    """Pre-populate the genes from the file included with the source code.
-    
-    NB: genes that are listed as synonyms also appear in their own right 
+    """Pre-populate the genes from a file.
+
+    There is a default Homo_sapiens.gene_info file included with the source code
+    or a custom file can be supplied.
+
+    NB: genes that are listed as synonyms also appear in their own right
     hence the code to check we don't already have a record.
     wc -l Homo_sapiens.gene_info == 43855
-    Gene.objects.filter(synonym_for__exact = None).count() == 43257"""
+    Gene.objects.filter(synonym_for__exact = None).count() == 43257
 
-    this_path = os.path.dirname(__file__)
-    file_list = ['Homo_sapiens.gene_info', ]
+    NB: Expect the first line contains headers not gene information.
+    """
+    genefile = open(settings.GENE_FILE_LOCATION, 'r')
 
-    for file in file_list:
-        genefile = open("%s/%s" % (this_path, file),'r')
+    headers = genefile.readline()
+    assert(headers.startswith("#Format:"))
 
-        for line in genefile:
-            line = line.strip().split()
-            genename = line[2]
-            if line[4] == "-":
-                synonyms = []
-            else:
-                synonyms = line[4].split("|")
+    for line in genefile:
+        line = line.strip().split()
+        genename = line[2]
+        if line[4] == "-":
+            synonyms = []
+        else:
+            synonyms = line[4].split("|")
 
-            # Store gene
-            if Gene.objects.filter(name=genename).exists():
-                this_gene = Gene.objects.get(name=genename)
-            else:
-                this_gene = Gene(name=genename)
-                this_gene.save()
+        # Store gene
+        if Gene.objects.filter(name=genename).exists():
+            this_gene = Gene.objects.get(name=genename)
+        else:
+            this_gene = Gene(name=genename)
+            this_gene.save()
 
-            # Store gene synonyms
-            for synonym in synonyms:
-                if not Gene.objects.filter(name=synonym).exists():
-                    new_synonym = Gene(name=synonym,
-                                       synonym_for=this_gene)
-                    new_synonym.save()
+        # Store gene synonyms
+        for synonym in synonyms:
+            if not Gene.objects.filter(name=synonym).exists():
+                new_synonym = Gene(name=synonym,
+                                   synonym_for=this_gene)
+                new_synonym.save()
 
-        genefile.close()
+    genefile.close()
