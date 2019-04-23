1219a02abb67bede47a51ef96385a632bb47dd11
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2019-04-23 19:45:25 +0100

    Extend tests for uoploading archives with a Ovid Medline formatted file

--- tests/test_searching.py
+++ tests/test_searching.py
@@ -78,6 +78,8 @@ TEST_BZ_PUB_MED_ARCHIVE = os.path.join(BASE_DIR, '10-40-56-prostatic_neoplasms.t
 TEST_GZIP_PUB_MED_ARCHIVE = os.path.join(BASE_DIR, '10-40-56-prostatic_neoplasms.txt.gz')
 TEST_BZ_PUB_MED_SMALL_ARCHIVE = os.path.join(BASE_DIR, 'pubmed_result_100.txt.bz2')
 TEST_GZIP_PUB_MED_SMALL_ARCHIVE = os.path.join(BASE_DIR, 'pubmed_result_100.txt.gz')
+TEST_BZ_OVID_ARCHIVE = os.path.join(BASE_DIR, 'ovid_result_100.txt.bz2')
+TEST_GZIP_OVID_ARCHIVE = os.path.join(BASE_DIR, 'ovid_result_100.txt.gz')
 
 #Invalid file uploads
 TEST_NO_MESH_SUBJECT_HEADINGS_FILE = os.path.join(BASE_DIR, 'no-mesh-terms-abstract.txt')
@@ -250,14 +252,11 @@ class SearchingTestCase(BaseTestCase):
         self._assert_upload_is_invalid_file_type(TEST_GZIP_DOC_ARCHIVE, search_path)
         self._assert_upload_is_invalid_file_type(TEST_BZ_DOC_ARCHIVE, search_path)
 
-    #TODO:Test medline in archives
-    #TODO Test archive with non text file inside
-    #TODO perform text for full search life cycle??
+    #TODO perform text for full search life cycle from small/large zip fie to matching
 
-    def _setup_file_upload_response(self, test_archive_file):
+    def _setup_file_upload_response(self, test_archive_file, search_path):
+        """Defaults to using pub med formatted search form"""
         self._login_user()
-        search_path = reverse('search_pubmed')
-
         with open(test_archive_file, 'r') as upload:
             response = self.client.post(search_path,
                                         {'abstracts_upload': upload,
@@ -265,10 +264,10 @@ class SearchingTestCase(BaseTestCase):
                                         follow=True)
         return response
 
-    def _assert_archive_file_is_uploaded_and_extracted(self, test_archive_file):
-        """Is this test file an archive we can process"""
+    def _assert_archive_file_is_uploaded_and_extracted(self, test_archive_file, search_path):
+        """Is this test file an archive we can process?"""
         previous_upload_count = Upload.objects.all().count()
-        response = self._setup_file_upload_response(test_archive_file)
+        response = self._setup_file_upload_response(test_archive_file, search_path)
         self.assertNotContains(response, "is not an acceptable file type")
         self.assertNotContains(response, "is not a plain text file")
         self.assertEqual(Upload.objects.all().count(), previous_upload_count + 1)
@@ -277,20 +276,26 @@ class SearchingTestCase(BaseTestCase):
         self.assertEqual(mime_type, "text/plain")
 
     def test_bz2_pub_med_upload_is_allowable(self):
-        self._assert_archive_file_is_uploaded_and_extracted(TEST_BZ_PUB_MED_ARCHIVE)
+        self._assert_archive_file_is_uploaded_and_extracted(TEST_BZ_PUB_MED_ARCHIVE, reverse('search_pubmed'))
 
     def test_gzip_pub_med_upload_is_allowable(self):
-        self._assert_archive_file_is_uploaded_and_extracted(TEST_GZIP_PUB_MED_ARCHIVE)
+        self._assert_archive_file_is_uploaded_and_extracted(TEST_GZIP_PUB_MED_ARCHIVE, reverse('search_pubmed'))
 
     def test_small_bz2_pub_med_upload_is_allowable(self):
-        self._assert_archive_file_is_uploaded_and_extracted(TEST_BZ_PUB_MED_SMALL_ARCHIVE)
+        self._assert_archive_file_is_uploaded_and_extracted(TEST_BZ_PUB_MED_SMALL_ARCHIVE, reverse('search_pubmed'))
 
     def test_small_gzip_pub_med_upload_is_allowable(self):
-        self._assert_archive_file_is_uploaded_and_extracted(TEST_GZIP_PUB_MED_SMALL_ARCHIVE)
+        self._assert_archive_file_is_uploaded_and_extracted(TEST_GZIP_PUB_MED_SMALL_ARCHIVE, reverse('search_pubmed'))
+
+    def test_bz2_ovid_upload_is_allowable(self):
+        self._assert_archive_file_is_uploaded_and_extracted(TEST_BZ_OVID_ARCHIVE, reverse('search_ovid_medline'))
+
+    def test_gzip_ovid_upload_is_allowable(self):
+        self._assert_archive_file_is_uploaded_and_extracted(TEST_GZIP_OVID_ARCHIVE, reverse('search_ovid_medline'))
 
     def _assert_invalid_pub_med_archive_fail(self, test_archive_file):
         previous_upload_count = Upload.objects.all().count()
-        response = self._setup_file_upload_response(test_archive_file)
+        response = self._setup_file_upload_response(test_archive_file, reverse('search_pubmed'))
         self.assertEqual(Upload.objects.all().count(), previous_upload_count)
         self.assertContains(response, "does not appear to be a PubMed/MEDLINE")
         self.assertNotContains(response, "is not an acceptable file type")
@@ -304,7 +309,7 @@ class SearchingTestCase(BaseTestCase):
 
     def test_small_zip_pub_med_is_unsupported(self):
         previous_upload_count = Upload.objects.all().count()
-        response = self._setup_file_upload_response(TEST_ZIP_PUB_MED_SMALL_ARCHIVE)
+        response = self._setup_file_upload_response(TEST_ZIP_PUB_MED_SMALL_ARCHIVE, reverse('search_pubmed'))
         self.assertEqual(Upload.objects.all().count(), previous_upload_count)
         self.assertContains(response, "is not an acceptable file type")
 
