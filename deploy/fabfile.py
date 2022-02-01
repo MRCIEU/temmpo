c6b1a90df60a4a315d034b586fa5a0acc91a4ed8
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2022-02-01 00:28:44 +0000

    Remove temporary clean up steps - required after migration of environments to using virtualenv

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -82,9 +82,6 @@ def make_virtualenv(env="dev", configure_apache=False, clone_repo=False, branch=
         stop_rqworker_service(use_local_mode)
 
     with change_dir(PROJECT_ROOT + 'lib/'):
-        # Added to work around issue found when using python -m venv command need to clear out old Python lib files for a clean virtualenv deployment.
-        caller('rm -rf %s/bin/' % venv_dir)
-        caller('ls -l %s' % venv_dir)
         caller('virtualenv-3.6 %s' % env)
 
         if clone_repo:
