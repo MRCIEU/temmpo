67441138e2d80706635ebb417557f4e753904a92
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2022-05-25 15:16:10 +0100

    [skip ci] Add debug output to fix for TMMA-433 chrome driver change

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -142,6 +142,7 @@ def make_virtualenv(env="dev", configure_apache=False, clone_repo=False, branch=
             caller('wget https://chromedriver.storage.googleapis.com/' + version + '/chromedriver_linux64.zip')
             caller('ls -l')
             caller('unzip -f -v chromedriver_linux64.zip')
+            caller('ls -l')
             caller('rm chromedriver_linux64.zip')
             caller('ls -l')
 
