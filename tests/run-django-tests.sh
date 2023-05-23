a008ca1b9fbf40f1019d72fc806aad9994542315
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2023-05-23 14:30:26 +0100

    TMMA-456: Use more recent version of pip and add more debug output

--- tests/run-django-tests.sh
+++ tests/run-django-tests.sh
@@ -21,20 +21,23 @@ mkdir -p var/results/testing/v4
 mkdir -p var/tmp
 
 
-# Check both versions of pip are installed
+echo "Check both versions of pip are installed"
 pip2 -V
 pip3 -V
 
-# Install Fabric
-pip2 install -U pip==19.3.1
+echo "Install Fabric"
+pip2 install -U pip==23.1.2
 pip2 install Fabric==1.13.1 # NB: v1.15.0 supports Python 2, & 3.6, 3.7, & 3.8
 
-# Install virtualenv
-which virtualenv
+echo "Install virtualenv"
 pip3 install virtualenv==20.13.0
+
+echo "Set up alias for virtualenv-3.8"
+which virtualenv
 alias virtualenv-3.8='virtualenv'
 which virtualenv-3.8
 
+echo "Set up alias for python3.8"
 which python3
 which python3.8
 alias python3.8="python3"
@@ -43,9 +46,10 @@ which python3.8
 cd $GITHUB_WORKSPACE
 cd lib/test/src/temmpo
 
-# Create virtualenv as per VM based environments
+echo "Create virtualenv as per VM based environments"
 fab make_virtualenv:env=test,configure_apache=False,clone_repo=False,branch=None,migrate_db=True,use_local_mode=True,requirements=test,restart_rqworker=False -f deploy/fabfile.py
 
+echo "Run coverage tests"
 # pip3 install -U pip==23.1.2
 # pip3 install setuptools==67.8.0
 # pip3 install pip-tools==6.13.0
@@ -54,6 +58,6 @@ fab make_virtualenv:env=test,configure_apache=False,clone_repo=False,branch=None
 # pip-sync requirements/test.txt
 # cd $GITHUB_WORKSPACE
 # cd lib/test/src/temmpo
-coverage
-coverage run --source='.' manage.py test --settings=temmpo.settings.test_mysql --exclude-tag=selenium-test --exclude-tag=skip-on-ubuntu
-coverage report --skip-empty --skip-covered -m
+../../bin/coverage
+../../bin/coverage run --source='.' manage.py test --settings=temmpo.settings.test_mysql --exclude-tag=selenium-test --exclude-tag=skip-on-ubuntu
+../../bin/coverage report --skip-empty --skip-covered -m
