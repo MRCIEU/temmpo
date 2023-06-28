d2827d14b568a9252a29dc9270c5fd22c7964ba8
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2023-06-28 16:29:25 +0100

    Bug fix path to Fabric file

--- tests/run-update-checks.sh
+++ tests/run-update-checks.sh
@@ -7,5 +7,5 @@ bash $GITHUB_WORKSPACE/tests/build-test-env.sh
 cd $GITHUB_WORKSPACE
 cd lib/test/src/temmpo
 
-echo "Generate updates"
-fab pip_sync_requirements_file:env=test,use_local_mode=True -f /usr/local/projects/temmpo/lib/dev/src/temmpo/deploy/fabfile.py
+echo "Generate any updates to requirements files"
+fab pip_sync_requirements_file:env=test,use_local_mode=True -f deploy/fabfile.py
