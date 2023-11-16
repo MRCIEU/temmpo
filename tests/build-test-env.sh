24a0b9833f7954dfb2731d8d29ec4bc23022c1d1
Tessa Alexander <1619343+asset-web@users.noreply.github.com>
2023-11-16 15:20:46 +0000

    TMMA-338: Upgrade deployment scripts to support python3.8 and Fabric 1.15.0 (#245)
    * TMMA-338: Upgrade Fabric in use and remove dependency for Python 2
    
    * TMMA-338: Update comments
    
    * TMMA-338: Bug fix installing fabric using python 3 for GitHub actions
    
    * TMMA-338: Remove installation of Python 2 in GitHub actions and update comments

--- tests/build-test-env.sh
+++ tests/build-test-env.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd $GITHUB_WORKSPACE
-# Move source code to a directory structure the application expects.
+echo "Move source code to a directory structure the application expects"
 mkdir -p lib/test/src/temmpo
 shopt -s extglob
 mv !(lib) lib/test/src/temmpo/
@@ -21,25 +21,26 @@ mkdir -p var/results/testing/v4
 mkdir -p var/tmp
 
 
-echo "Check both versions of pip are installed"
-pip2 -V
+echo "Check the version of pip that is installed"
+pip3 -V
+echo "Ensure using a consistent version of pip as per on premises CI server"
+pip3 install -U pip==22.0.2
 pip3 -V
 
 echo "Install Fabric"
-pip2 install -U pip==20.3.4
-pip2 install Fabric==1.13.1 # NB: v1.15.0 supports Python 2, & 3.6, 3.7, & 3.8
+pip3 install fabric==1.15.0 # NB: v1.15.0 supports Python 2, & 3.6, 3.7, & 3.8
 
 echo "Install virtualenv"
-pip3 install virtualenv==20.13.0
+pip3 install virtualenv==15.1.0
 
 echo "Install wheel"
 sudo apt-get install -y python3-wheel-whl
 
-echo "Set up alias for python3.8"
+echo "Check aliases for python3.8"
 which python3
 which python3.8
 
-# Install Chrome
+echo "Install Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 rm -f google-chrome-stable_current_amd64.deb
