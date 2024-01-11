3b430edbd85adb9d78810d6f6e81670c3ebc6557
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2024-01-11 12:56:39 +0000

    [skip ci] WIP add help Fabric scripts for automating mesh term importation

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -664,3 +664,48 @@ def remove_incompleted_registrations(env="demo", use_local_mode=False, project_d
 
     with change_dir(src_dir):
         caller('%sbin/python3 manage.py remove_incompleted_registrations --settings=temmpo.settings.%s' % (venv_dir, env))
+
+def import_mesh_terms(env="dev", use_local_mode=False, project_dir=PROJECT_ROOT, year=None):
+    """env=dev,use_local_mode=False,mesh_term_file_path="",year=None"""
+    use_local_mode = (str(use_local_mode).lower() == 'true')
+    # Allow function to be run locally or remotely
+    caller, change_dir = _toggle_local_remote(use_local_mode)
+    venv_dir = project_dir + "lib/" + env + "/"
+    src_dir = project_dir + "lib/" + env + "/src/temmpo/"
+    if not year:
+        year = str(datetime.now().year)
+    mesh_term_file_path = f'./temmpo/prepopulate/mtrees{year}.bin'
+
+    with change_dir(src_dir):
+        caller('echo "START: Import mesh terms for year: {year} with file: {mesh_term_file_path}."')
+        caller(f'{venv_dir}bin/python3 manage.py import_mesh_terms {mesh_term_file_path} {year} --settings=temmpo.settings.{env}')
+        caller('echo "END: Import mesh terms for year: {year} with file: {mesh_term_file_path}."')
+
+def create_mesh_term_fixtures(env="dev", use_local_mode=False, project_dir=PROJECT_ROOT, previous_fixture_file="", new_fixture_file="mesh_terms.json", mesh_term_file_path="", year=None):
+    """
+    Download bin file
+    Import previous mesh term file fixture
+    Import mesh term into test (dev) instance
+    Dump mesh term fixtures
+    TODO: Wire to a GitHub action to create a pull request/save archive files
+        add *.bin file
+        add `mesh_terms.json` fixture file
+    """
+    use_local_mode = (str(use_local_mode).lower() == 'true')
+    # Allow function to be run locally or remotely
+    caller, change_dir = _toggle_local_remote(use_local_mode)
+    venv_dir = project_dir + "lib/" + env + "/"
+    src_dir = project_dir + "lib/" + env + "/src/temmpo/"
+    if not year:
+        year = str(datetime.now().year)
+    mesh_term_file_path = f'./temmpo/prepopulate/mtrees{year}.bin'
+    new_fixture_file_path = f'./browser/fixtures/{new_fixture_file}'
+
+    with change_dir(src_dir):
+        caller(f'wget -O {mesh_term_file_path} https://nlmpubs.nlm.nih.gov/projects/mesh/{year}/meshtrees/mtrees{year}.bin')
+        if _exists_local(mesh_term_file_path, use_local_mode):
+            caller(f'{venv_dir}bin/python3 manage.py loaddata {previous_fixture_file} --settings=temmpo.settings.{env}')
+            caller(f'{venv_dir}bin/python3 manage.py import_mesh_terms {mesh_term_file_path} {year} --settings=temmpo.settings.{env}')
+            caller(f'{venv_dir}bin/python3 manage.py dumpdata dumpdata browser.MeshTerm --indent 4 --output {new_fixture_file_path} --settings=temmpo.settings.{env}')
+        else:
+            caller(f'echo "ERROR: Could not download mesh terms for year: {year}".')
