db83a42a1b87be5b9eade98cb7c35891a2cbc173
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2024-01-12 12:17:17 +0000

    TMMA-273: Automate importing new MeshTerm terms - Bug fix paths used in GitHub action

--- tests/check-for-new-mesh-terms.sh
+++ tests/check-for-new-mesh-terms.sh
@@ -5,8 +5,17 @@ set -e
 bash $GITHUB_WORKSPACE/tests/build-test-env.sh
 
 cd $GITHUB_WORKSPACE
-
+cd lib/test/src/temmpo
 # One off previous year - TODO: Remove once this legacy import has been created
 fab create_mesh_term_fixtures:env=test,use_local_mode=True,previous_fixture_file=mesh_terms.json,year=2023 -f deploy/fabfile.py
 # current year - TODO: Enable once the legacy import has been created
 # fab create_mesh_term_fixtures:env=test,use_local_mode=True,previous_fixture_file=mesh_terms.json,year=`date +"%Y"` -f deploy/fabfile.py
+
+# Move code back to expected location for pull request
+cd $GITHUB_WORKSPACE
+
+ls lib/test/src/temmpo
+mv lib/test/src/temmpo/* $GITHUB_WORKSPACE
+ls
+ls -l browser/fixtures
+ls -l temmpo/prepopulate
