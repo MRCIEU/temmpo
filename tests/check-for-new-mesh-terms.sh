1c56fce502c15db32e72e2387976c688fdb51e0d
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2024-01-12 12:23:35 +0000

    Ensure update project directory for GitHub actions usage of fabric command

--- tests/check-for-new-mesh-terms.sh
+++ tests/check-for-new-mesh-terms.sh
@@ -7,7 +7,7 @@ bash $GITHUB_WORKSPACE/tests/build-test-env.sh
 cd $GITHUB_WORKSPACE
 cd lib/test/src/temmpo
 # One off previous year - TODO: Remove once this legacy import has been created
-fab create_mesh_term_fixtures:env=test,use_local_mode=True,previous_fixture_file=mesh_terms.json,year=2023 -f deploy/fabfile.py
+fab create_mesh_term_fixtures:env=test,use_local_mode=True,project_dir=$GITHUB_WORKSPACE/,previous_fixture_file=mesh_terms.json,year=2023 -f deploy/fabfile.py
 # current year - TODO: Enable once the legacy import has been created
 # fab create_mesh_term_fixtures:env=test,use_local_mode=True,previous_fixture_file=mesh_terms.json,year=`date +"%Y"` -f deploy/fabfile.py
 
