d00f976af414fbcab40bbde3d8c2e1fe860fc183
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2024-02-09 16:22:08 +0000

    Increase loogging level for more debug output

--- temmpo/settings/test_mysql.py
+++ temmpo/settings/test_mysql.py
@@ -14,7 +14,7 @@ if DATABASES:
 
 RQ_QUEUES['default']['ASYNC'] = False
 
-LOGGING['handlers']['console']['level'] = 'ERROR'
+LOGGING['handlers']['console']['level'] = 'INFO'
 
 RESULTS_PATH_V4 = os.path.join(MEDIA_ROOT, 'results', 'testing', 'v4', '')
 RESULTS_PATH_V3 = os.path.join(MEDIA_ROOT, 'results', 'testing', 'v3', '')
