32fb700536f2b1dc4da68baf66f4871500943a5d
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2024-02-09 16:17:51 +0000

    TMMA-436: Update Django usage based on feedback from django-upgrade feedback

--- temmpo/urls.py
+++ temmpo/urls.py
@@ -25,51 +25,51 @@ from browser.views import (HomeView, CreditsView, HelpView, SearchOvidMEDLINE, R
 urlpatterns = [
 
     # browser app
-    re_path(r'^$', HomeView.as_view(), name='home'),
-    re_path(r'^credits/$', CreditsView.as_view(), name='credits'),
-    re_path(r'^help/$', HelpView.as_view(), name='help'),
-    re_path(r'^privacy/$', PrivacyPolicyView.as_view(), name="privacy"),
+    path('', HomeView.as_view(), name='home'),
+    path('credits/', CreditsView.as_view(), name='credits'),
+    path('help/', HelpView.as_view(), name='help'),
+    path('privacy/', PrivacyPolicyView.as_view(), name="privacy"),
 
-    re_path(r'^search/$', SelectSearchTypeView.as_view(), name='search'),
-    re_path(r'^search/select/$', ReuseSearchView.as_view(), name='reuse_search'),
-    re_path(r'^search/ovidmedline/$', SearchOvidMEDLINE.as_view(), name='search_ovid_medline'),
-    re_path(r'^search/pubmed/$', SearchPubMedView.as_view(), name='search_pubmed'),
-    re_path(r'^search/edit/(?P<pk>\d+)/$', SearchExisting.as_view(), name="edit_search"),  # Create a new search based on an existing search criteria
-    re_path(r'^search/reuse/(?P<pk>\d+)/$', SearchExistingUpload.as_view(), name="reuse_upload"),  # Create a new search based on previously uploaded set of abstracts
-    re_path(r'^exposure/(?P<pk>\d+)/$', ExposureSelector.as_view(), name="exposure_selector"),
-    re_path(r'^mediator/(?P<pk>\d+)/$', MediatorSelector.as_view(), name="mediator_selector"),
-    re_path(r'^outcome/(?P<pk>\d+)/$', OutcomeSelector.as_view(), name="outcome_selector"),
-    re_path(r'^filter/(?P<pk>\d+)/$', FilterSelector.as_view(), name="filter_selector"),
+    path('search/', SelectSearchTypeView.as_view(), name='search'),
+    path('search/select/', ReuseSearchView.as_view(), name='reuse_search'),
+    path('search/ovidmedline/', SearchOvidMEDLINE.as_view(), name='search_ovid_medline'),
+    path('search/pubmed/', SearchPubMedView.as_view(), name='search_pubmed'),
+    path('search/edit/<int:pk>/', SearchExisting.as_view(), name="edit_search"),  # Create a new search based on an existing search criteria
+    path('search/reuse/<int:pk>/', SearchExistingUpload.as_view(), name="reuse_upload"),  # Create a new search based on previously uploaded set of abstracts
+    path('exposure/<int:pk>/', ExposureSelector.as_view(), name="exposure_selector"),
+    path('mediator/<int:pk>/', MediatorSelector.as_view(), name="mediator_selector"),
+    path('outcome/<int:pk>/', OutcomeSelector.as_view(), name="outcome_selector"),
+    path('filter/<int:pk>/', FilterSelector.as_view(), name="filter_selector"),
 
-    re_path(r'^mesh-terms-json/$', cache_page(60 * 60 * 24 * 355)(MeshTermsAllAsJSON.as_view()), name="mesh_terms_as_json"),
-    re_path(r'^mesh_terms_search_json/$', MeshTermSearchJSON.as_view(), name="mesh_terms_search_json"),
+    path('mesh-terms-json/', cache_page(60 * 60 * 24 * 355)(MeshTermsAllAsJSON.as_view()), name="mesh_terms_as_json"),
+    path('mesh_terms_search_json/', MeshTermSearchJSON.as_view(), name="mesh_terms_search_json"),
     re_path(r'^mesh-terms-json-for-criteria/(?P<pk>\d+)/(?P<type>(exposure|mediator|outcome))/$', MeshTermsAsJSON.as_view(), name="mesh_terms_as_json_for_criteria"),
 
-    re_path(r'^results/(?P<pk>\d+)/$', ResultsSankeyView.as_view(), name='results'),
-    re_path(r'^results/bubble/(?P<pk>\d+)/$', ResultsBubbleView.as_view(), name='results_bubble'),
-    re_path(r'^results/$', ResultsListingView.as_view(), name='results_listing'),
+    path('results/<int:pk>/', ResultsSankeyView.as_view(), name='results'),
+    path('results/bubble/<int:pk>/', ResultsBubbleView.as_view(), name='results_bubble'),
+    path('results/', ResultsListingView.as_view(), name='results_listing'),
 
-    re_path(r'^search-criteria/(?P<pk>\d+)/$', CriteriaView.as_view(), name='criteria'),
+    path('search-criteria/<int:pk>/', CriteriaView.as_view(), name='criteria'),
 
-    re_path(r'^data/delete/(?P<pk>\d+)/$', DeleteSearch.as_view(), name='delete_data'),
+    path('data/delete/<int:pk>/', DeleteSearch.as_view(), name='delete_data'),
 
-    re_path(r'^data/v4/count/(?P<pk>\d+)/$', CountDataView.as_view(), name='count_data'),
-    re_path(r'^data/v4/abstracts/(?P<pk>\d+)/$', AbstractDataView.as_view(), name='abstracts_data'),
-    re_path(r'^data/v4/json/(?P<pk>\d+)/$', JSONDataView.as_view(), name='json_data'),
+    path('data/v4/count/<int:pk>/', CountDataView.as_view(), name='count_data'),
+    path('data/v4/abstracts/<int:pk>/', AbstractDataView.as_view(), name='abstracts_data'),
+    path('data/v4/json/<int:pk>/', JSONDataView.as_view(), name='json_data'),
 
-    re_path(r'^data/v3/count/(?P<pk>\d+)/$', CountDataViewV3.as_view(), name='count_data_v3'),
-    re_path(r'^data/v3/abstracts/(?P<pk>\d+)/$', AbstractDataViewV3.as_view(), name='abstracts_data_v3'),
-    re_path(r'^data/v3/json/(?P<pk>\d+)/$', JSONDataViewV3.as_view(), name='json_data_v3'),
+    path('data/v3/count/<int:pk>/', CountDataViewV3.as_view(), name='count_data_v3'),
+    path('data/v3/abstracts/<int:pk>/', AbstractDataViewV3.as_view(), name='abstracts_data_v3'),
+    path('data/v3/json/<int:pk>/', JSONDataViewV3.as_view(), name='json_data_v3'),
 
-    re_path(r'^data/v1/count/(?P<pk>\d+)/$', CountDataViewV1.as_view(), name='count_data_v1'),
-    re_path(r'^data/v1/abstracts/(?P<pk>\d+)/$', AbstractDataViewV1.as_view(), name='abstracts_data_v1'),
-    re_path(r'^data/v1/json/(?P<pk>\d+)/$', JSONDataViewV1.as_view(), name='json_data_v1'),
+    path('data/v1/count/<int:pk>/', CountDataViewV1.as_view(), name='count_data_v1'),
+    path('data/v1/abstracts/<int:pk>/', AbstractDataViewV1.as_view(), name='abstracts_data_v1'),
+    path('data/v1/json/<int:pk>/', JSONDataViewV1.as_view(), name='json_data_v1'),
 
-    re_path(r'^account/$', UserAccountView.as_view(), name='account'),
-    re_path(r'^close-account/(?P<pk>\d+)/$', CloseAccount.as_view(), name='close_account'),
-    re_path(r'^account-closed/$', AccountClosedConfirmation.as_view(), name='account_closed'),
-    re_path(r'^manage-users/$', UsersListingView.as_view(), name='manage_users'),
-    re_path(r'^delete-user/(?P<pk>\d+)/$', DeleteUser.as_view(), name='delete_user'),
+    path('account/', UserAccountView.as_view(), name='account'),
+    path('close-account/<int:pk>/', CloseAccount.as_view(), name='close_account'),
+    path('account-closed/', AccountClosedConfirmation.as_view(), name='account_closed'),
+    path('manage-users/', UsersListingView.as_view(), name='manage_users'),
+    path('delete-user/<int:pk>/', DeleteUser.as_view(), name='delete_user'),
 
     # Favicon
     path(
@@ -78,7 +78,7 @@ urlpatterns = [
     ),
 
     # Probe page
-    re_path(r'^probe/$',
+    path('probe/',
         cache_page(15)(ProbeView.as_view()),
         name='probe'),
 
@@ -86,15 +86,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Django user authentication
-    re_path(r'^logout/$', LogoutView.as_view(), name="logout"),
-    re_path(r'^', include('registration.backends.default.urls')),
-    re_path(r'^', include('django.contrib.auth.urls')),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('', include('registration.backends.default.urls')),
+    path('', include('django.contrib.auth.urls')),
 
     # django-rq Redis backed message queue
-    re_path(r'^django-rq/', include('django_rq.urls')),
+    path('django-rq/', include('django_rq.urls')),
 
     # autocomplete
-    re_path(r'^meshterm-autocomplete/$', MeSHTermAutocomplete.as_view(), name='meshterm-autocomplete'),
+    path('meshterm-autocomplete/', MeSHTermAutocomplete.as_view(), name='meshterm-autocomplete'),
 ]
 
 # For non Apache fronted Django development server scenarios.
@@ -110,5 +110,5 @@ if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
         urlpatterns += [
-            re_path(r'^__debug__/', include(debug_toolbar.urls)),
+            path('__debug__/', include(debug_toolbar.urls)),
         ]
