3a1334b6cae60ff635ef614ac93c4e0c7a2e3eec
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2024-11-05 14:06:24 +0000

    Update Probe page test

--- browser/probe.py
+++ browser/probe.py
@@ -99,7 +99,7 @@ class ProbeView(TemplateView):
             return True
 
     def python_version(self):
-        if sys.version_info.major == 3 and sys.version_info.minor == 9:
+        if sys.version_info.major == 3 and sys.version_info.minor == 12:
             return True
         else:
             return False
