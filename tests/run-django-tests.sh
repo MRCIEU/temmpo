8ef7088ab5aa5cd0c697c6ac85c3657f90151857
Tessa Alexander <1619343+asset-web@users.noreply.github.com>
2024-12-18 20:57:52 +0000

    Run tests in parallel; Harmonise test environments (#336)

--- tests/run-django-tests.sh
+++ tests/run-django-tests.sh
@@ -8,5 +8,5 @@ echo "Run coverage tests"
 cd $GITHUB_WORKSPACE
 cd lib/test/src/temmpo
 ../../bin/coverage
-../../bin/coverage run --source='.' manage.py test --settings=temmpo.settings.test_mysql --exclude-tag=selenium-test
+../../bin/coverage run --source='.' manage.py test --parallel --settings=temmpo.settings.test_mysql --exclude-tag=selenium-test
 ../../bin/coverage report --skip-empty --skip-covered -m
