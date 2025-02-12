c4bf3474072725dd4df6becb957ea32b03c78833
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2025-02-12 12:07:10 +0000

    Update test environment and packaging tools

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -18,7 +18,7 @@ GIT_SSH_HOSTS = ('bitbucket.org',
 
 # Tools not handled by pip-tools and/or requirements installs using pip
 # Also update pip version in tests/build-test-env.sh and Dockerfile
-PIP_VERSION = '24.3.1'
+PIP_VERSION = '25.0.1'
 SETUPTOOLS_VERSION = '75.8.0'
 PIP_TOOLS_VERSION = '7.4.1'
 
